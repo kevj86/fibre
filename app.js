50e32b37dd9187847105bbd0cc9a709ac806f549
kevj86 <109628697+kevj86@users.noreply.github.com>
2024-08-22 21:09:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // DATABASE INFO
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove, set } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove, set } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://fibre-b9004-default-rtdb.europe-west1.firebasedatabase.app/",
-};
+}
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const itemCodes = ref(database, "items");
-const displayResults = document.getElementById("display-results");
-let displayHTML = "";
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemCodes = ref(database, "items")
+const displayResults = document.getElementById("display-results")
+let displayHTML = ""
 
 // ITEM CODES JS
 
@@ -25,47 +25,49 @@ const selectStoreType = `
         <option value="consumables" data-button="consumables">Consumables</option>
         <option value="tools" data-button="tools">Tools</option>
         <option value="misc" data-button="misc">Misc</option>
-      </select>`;
+      </select>`
 
-let selected = "";
+let selected = ""
 
 if (window.location.href === "https://kjfibre.netlify.app/stores") {
   onValue(itemCodes, function (snapshot) {
-    selected = document.getElementById("display-items").options[document.getElementById("display-items").selectedIndex].getAttribute("data-button");
-    console.log(selected);
-    const newItemList = [];
-    let itemList = Object.entries(snapshot.val());
+    selected = document
+      .getElementById("display-items")
+      .options[document.getElementById("display-items").selectedIndex].getAttribute("data-button")
+    console.log(selected)
+    const newItemList = []
+    let itemList = Object.entries(snapshot.val())
 
     for (let i = 0; i < itemList.length; i++) {
-      let newKey = itemList[i][0];
-      let newEntry = itemList[i][1];
+      let newKey = itemList[i][0]
+      let newEntry = itemList[i][1]
 
       newItemList.push({
         key: newKey,
         itemname: newEntry.itemname,
         itemcode: newEntry.itemcode,
         storetype: newEntry.storetype,
-      });
+      })
     }
 
     document.addEventListener("change", function (e) {
-      console.log(e.target.value);
-      displayHTML = "";
+      console.log(e.target.value)
+      displayHTML = ""
       const filterItems = newItemList.filter((store) => {
-        return store.storetype === e.target.value;
-      });
-      render(filterItems);
-      document.getElementById("display-results").innerHTML = displayHTML;
-    });
+        return store.storetype === e.target.value
+      })
+      render(filterItems)
+      document.getElementById("display-results").innerHTML = displayHTML
+    })
 
     const filterItems = newItemList.filter((store) => {
-      return store.storetype === selected;
-    });
+      return store.storetype === selected
+    })
 
-    render(filterItems);
+    render(filterItems)
 
-    document.getElementById("display-results").innerHTML = displayHTML;
-  });
+    document.getElementById("display-results").innerHTML = displayHTML
+  })
 
   function render(filterItems) {
     filterItems.forEach((filteredItem) => {
@@ -76,136 +78,158 @@ if (window.location.href === "https://kjfibre.netlify.app/stores") {
                 <div>${filteredItem.itemcode}</div>
               </div>
             </div>
-          `;
-    });
+          `
+    })
   }
 }
 
 // FIBRE COLOUR CODES
 
-let elementSize = null;
-let fibreSelect = null;
+let elementSize = null
+let fibreSelect = null
 
-const maxElementFibre = 12;
-const maxElementFibreEight = 8;
+const maxElementFibre = 12
+const maxElementFibreEight = 8
 
-const fibreColours = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple", "black", "white", "pink", "aqua"];
-const fibreColoursEight = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple"];
+const fibreColours = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple", "black", "white", "pink", "aqua"]
+const fibreColoursEight = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple"]
 
 // DOM Selectors
 
-const fibreNumber = document.getElementById("fibre-number");
-const elementNumber = document.getElementById("fibre-element");
-const displayFibre = document.getElementById("display-fibre");
-const displayElement = document.getElementById("display-element");
-const twelveFibre = document.getElementById("12f");
-const eightFibre = document.getElementById("8f");
+const fibreNumber = document.getElementById("fibre-number")
+const elementNumber = document.getElementById("fibre-element")
+const displayFibre = document.getElementById("display-fibre")
+const displayElement = document.getElementById("display-element")
+const twelveFibre = document.getElementById("12f")
+const eightFibre = document.getElementById("8f")
 
 function getFibreNumber() {
-  const fibre = fibreNumber.value;
-  const elementValue = Math.ceil(fibre / 12);
+  const fibre = fibreNumber.value
+  const elementValue = Math.ceil(fibre / 12)
 
-  let fibreColour = fibre < 13 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 12 - 1;
+  let fibreColour = fibre < 13 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 12 - 1
 
   let coloursHtml = fibreColours
     .map((fibreColour, index) => {
-      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibre * elementValue - (11 - index)}</span> `;
+      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibre * elementValue - (11 - index)}</span> `
     })
-    .join("");
+    .join("")
 
   if (fibreNumber.value === "" || fibreNumber.value === "0") {
   } else if (fibreNumber.value > 864) {
     displayFibre.innerHTML = `
         <div class="alert">MAX FIBRE COUNT 864</div>
-    `;
+    `
   } else {
     displayFibre.innerHTML = `
         <div>Fibre <span class="accent">${fibre}<span> is  <span class="${fibreColours[fibreColour]}-fibre inline-span">${fibreColours[fibreColour]}</span></div>
         <div>and is in ELEMENT <span class="accent">${elementValue}</span></div>
-        <div class="colour-display">${coloursHtml}</div>`;
+        <div class="colour-display">${coloursHtml}</div>`
   }
 }
 
 function getFibreNumberEight() {
-  const fibre = fibreNumber.value;
-  const elementValue = Math.ceil(fibre / 8);
+  const fibre = fibreNumber.value
+  const elementValue = Math.ceil(fibre / 8)
 
-  let fibreColour = fibre < 9 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 8 - 1;
+  let fibreColour = fibre < 9 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 8 - 1
 
   let coloursHtml = fibreColoursEight
     .map((fibreColour, index) => {
-      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibreEight * elementValue - (7 - index)}</span> `;
+      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibreEight * elementValue - (7 - index)}</span> `
     })
-    .join("");
+    .join("")
 
   if (fibreNumber.value === "" || fibreNumber.value === "0") {
   } else if (fibreNumber.value > 96) {
     displayFibre.innerHTML = `
         <div class="alert">MAX FIBRE COUNT 96</div>
-    `;
+    `
   } else {
     displayFibre.innerHTML = `
         <div>Fibre <span class="accent">${fibre}<span> is  <span class="${fibreColoursEight[fibreColour]}-fibre inline-span">${fibreColoursEight[fibreColour]}</span></div>
         <div>and is in ELEMENT <span class="accent">${elementValue}</span></div>
-        <div class="colour-display">${coloursHtml}</div>`;
+        <div class="colour-display">${coloursHtml}</div>`
   }
 }
 
 function getElementNumber() {
   let coloursHtml = fibreColours
     .map((fibreColour, index) => {
-      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibre * elementNumber.value - (11 - index)}</span> `;
+      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibre * elementNumber.value - (11 - index)}</span> `
     })
-    .join("");
+    .join("")
 
   if (elementNumber.value === "" || elementNumber === "0") {
   } else if (elementNumber.value > 72) {
-    displayElement.innerHTML = `<div id="element-Count" class="alert">MAX ELEMENT COUNT 72</div>`;
+    displayElement.innerHTML = `<div id="element-Count" class="alert">MAX ELEMENT COUNT 72</div>`
   } else {
     displayElement.innerHTML = `
       <div>Fibre numbers in ELEMENT <span class="accent">${elementNumber.value}</span></div>
-      <div class="colour-display">${coloursHtml}</div>`;
+      <div class="colour-display">${coloursHtml}</div>`
   }
 }
+
+function getElementNumberEight() {
+  let coloursHtml = fibreColoursEight
+    .map((fibreColour, index) => {
+      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibreEight * elementNumber.value - (7 - index)}</span> `
+    })
+    .join("")
+
+  if (elementNumber.value === "" || elementNumber === "0") {
+  } else if (elementNumber.value > 12) {
+    displayElement.innerHTML = `<div id="element-Count" class="alert">MAX ELEMENT COUNT 12</div>`
+  } else {
+    displayElement.innerHTML = `
+      <div>Fibre numbers in ELEMENT <span class="accent">${elementNumber.value}</span></div>
+      <div class="colour-display">${coloursHtml}</div>`
+  }
+}
+
 // Reset Content
 
 function resetValues() {
-  fibreNumber.value = "";
-  elementNumber.value = "";
-  displayElement.innerHTML = "";
-  displayFibre.innerHTML = "";
+  fibreNumber.value = ""
+  elementNumber.value = ""
+  displayElement.innerHTML = ""
+  displayFibre.innerHTML = ""
 }
 
 // Event Listeners
 
 fibreNumber?.addEventListener("keyup", function () {
-  fibreNumber.value === "" ? resetValues() : null;
+  fibreNumber.value === "" ? resetValues() : null
 
   if (fibreSelect === 12) {
-    getFibreNumber();
+    getFibreNumber()
   } else if (fibreSelect === 8) {
-    getFibreNumberEight();
+    getFibreNumberEight()
   }
-});
+})
 
 elementNumber?.addEventListener("keyup", function () {
-  getElementNumber();
-});
+  if (fibreSelect === 12) {
+    getElementNumber()
+  } else if (fibreSelect === 8) {
+    getElementNumberEight()
+  }
+})
 
 document.getElementById("reset")?.addEventListener("click", () => {
-  resetValues();
-});
+  resetValues()
+})
 
 twelveFibre?.addEventListener("click", function () {
-  fibreSelect = 12;
-  resetValues();
-  eightFibre.classList.remove("active");
-  twelveFibre.classList.add("active");
-});
+  fibreSelect = 12
+  resetValues()
+  eightFibre.classList.remove("active")
+  twelveFibre.classList.add("active")
+})
 
 eightFibre?.addEventListener("click", function () {
-  fibreSelect = 8;
-  resetValues();
-  eightFibre.classList.add("active");
-  twelveFibre.classList.remove("active");
-});
+  fibreSelect = 8
+  resetValues()
+  eightFibre.classList.add("active")
+  twelveFibre.classList.remove("active")
+})
